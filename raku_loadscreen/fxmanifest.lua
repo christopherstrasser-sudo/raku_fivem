cc8c93d4de120a2c7e62338bca08bc1a97617199
christopherstrasser-sudo <christopher.strasser@gmx.de>
2026-09-04 08:37:22 +0200

    Add Raku loading screen manifest

---
+++ raku_loadscreen/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'raku_loadscreen'
+author 'Raku / OpenAI'
+description 'Custom FiveM loading screen for Raku Roleplay'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
