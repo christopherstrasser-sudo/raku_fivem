21bf704d55621921302a4cdaf8ad0519a57b455b
christopherstrasser-sudo <christopher.strasser@gmx.de>
2026-09-04 08:34:26 +0200

    Add Raku Dev Bridge test resource manifest

---
+++ raku_bridge_test/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'raku_bridge_test'
+author 'Raku / OpenAI'
+description 'Minimal test resource for the Raku Dev Bridge'
+version '1.0.0'
+
+server_script 'server.lua'
